9f58b788849d907b352e74a2649af579ea0076ff
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-07-18 18:45:52 +0200

    [UPD] created object array as data

--- js/script.js
+++ js/script.js
@@ -3,7 +3,27 @@ const {createApp} = Vue;
 createApp ({
     data () {
         return {
-
+            todolist: [
+                {
+                    text: 'Andare a fare la spesa',
+                    done: false,
+                },{
+                    text: 'Passeggiata con il cane',
+                    done: true,
+                },{
+                    text: 'Comprare il giornale',
+                    done: true,
+                },{
+                    text: 'Portare fuori la spazzatura',
+                    done: true,
+                },{
+                    text: 'Finire l\'esercizio del giorno',
+                    done: true,
+                },{
+                    text: 'Preparare la cena',
+                    done: false
+                }
+            ]
         }
     }
 }).mount('#app')
